84badd2b8312210abb5960d892d31d8c0168b876
wabernathy96 <wabernathy96@gmail.com>
2018-07-09 13:52:13 -0500

    Heroku init

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -63,6 +63,25 @@ $(document).ready(function() {
     }
   });
 
+  // Wrap every letter in a span
+  $(".ml16").each(function() {
+    $(this).html(
+      $(this)
+        .text()
+        .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
+    );
+  });
+
+  anime.timeline({ loop: false }).add({
+    targets: ".ml16 .letter",
+    translateY: [-100, 0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: function(el, i) {
+      return 30 * i;
+    }
+  });
+
   // $(".pl12").each(function() {
   //   $(this).html(
   //     $(this)
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,7 @@ const routes = express.Router();
 routes.get("/", (req, res) => res.render("landing"));
 routes.get("/home", (req, res) => res.render("home"));
 routes.get("/about", (req, res) => res.render("about"));
+routes.get("/about/work-history", (req, res) => res.render("workhistory"));
+routes.get("/about/off-clock", (req, res) => res.render("offclock"));
 
 module.exports = routes;
