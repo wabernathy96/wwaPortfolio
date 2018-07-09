325382c4d5fb4089429005f81c3e92b8595d5354
wabernathy96 <wabernathy96@gmail.com>
2018-07-08 22:52:32 -0500

    About page done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -63,6 +63,27 @@ $(document).ready(function() {
     }
   });
 
+  // $(".pl12").each(function() {
+  //   $(this).html(
+  //     $(this)
+  //       .text()
+  //       .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
+  //   );
+  // });
+
+  // // New Production Animation - About paragraph
+  // anime.timeline({ loop: false }).add({
+  //   targets: ".pl12 .letter",
+  //   translateX: [40, 0],
+  //   translateZ: 0,
+  //   opacity: [0, 1],
+  //   easing: "easeOutExpo",
+  //   duration: 3000,
+  //   delay: function(el, i) {
+  //     return 500 + 30 * i;
+  //   }
+  // });
+
   // $(".btn").click(function() {
   //   anime.timeline({ loop: false }).add({
   //     targets: ".btn",
